813515b39f12f023528dc0cc4f6123960fec9067
Rahmi Pruitt <rjpruitt16@gmail.com>
2018-01-21 23:04:24 -0500

    Add Link Extraction function for NPR

--- opinions_scrape.py
+++ opinions_scrape.py
@@ -2,7 +2,7 @@
 import bs4 as bs
 from urllib.request import Request, urlopen
 from newspaper import Article
-from nlu import getSentiment
+
 def getArticle(url):
   article = Article(url)
   article.download()
@@ -22,33 +22,44 @@ def writeSoupToTextFile(path, soup, article_sauce):
       text_file.write(paragraph.text)
 
 
-def getFirstUrlSoup(sitemap):
-  sauce = Request(sitemap, headers={"User-Agent": "Mozilla"})
+def makeUrlSoup(url):
+  sauce = Request(url, headers={"User-Agent": "Mozilla"})
   webpage = urlopen(sauce).read()
   soup = bs.BeautifulSoup(webpage, 'lxml')
   return soup
 
-def writeArticleToTextFile(article, path, sentiment):
+def writeArticleToTextFile(article, path, keywords):
   article.download()
   with open(path, "w") as text_file:
-    text_file.write('{}\n{}\n{} {}'.format(article.url, article.text.replace('\n\n', ' '), sentiment[0], sentiment[1]))
+    text_file.write('{}\n{}\n{}\n{}'.format(article.url, article.text.replace('\n\n', ' '), keywords, str(article)))
 
-def FindAndWriteArticle(sitemap, article_name):
-  soup = getFirstUrlSoup(sitemap)
-  url = soup.find_all('loc')[0].text
+def getListOfArticleLinks(url, soup):
+    ## A function to get Article links from front page.
+    for article in soup.find_all('article'):
+        return article.find_all('a', href=True)
+
+def FindAndWriteArticle(sitemap="", front_page="", article_name=""):
+  soup = ""
+  url = ""
+  if sitemap:
+    soup = makeUrlSoup(sitemap)
+    url = soup.find_all('loc')[0].text
+  elif front_page:
+    soup = makeUrlSoup(front_page)
+    url = getListOfArticleLinks(front_page, soup)[0]["href"]
+    print(url)
   article = getArticle(url)
-  sentiment = getSentimemt(article)
-  writeArticleToTextFile(article, "article_samples/" + article_name, sentiment)
-  
+  writeArticleToTextFile(article, "article_samples/" + article_name, article.keywords)
+
 if __name__ == "__main__":
-  FindAndWriteArticle("https://www.washingtonpost.com/news-opinions-sitemap.xml", 'washington_article.txt')
+  ##FindAndWriteArticle("https://www.washingtonpost.com/news-opinions-sitemap.xml", 'washington_article.txt')
 
-  FindAndWriteArticle("http://www.cnn.com/sitemaps/sitemap-articles-2017-11.xml", "cnn_article.txt")
+  ##FindAndWriteArticle("http://www.cnn.com/sitemaps/sitemap-articles-2017-11.xml", "cnn_article.txt")
 
-  FindAndWriteArticle("https://www.huffingtonpost.com/sitemap.xml", "huffington_article.txt")
+  ##FindAndWriteArticle("https://www.huffingtonpost.com/sitemap.xml", "huffington_article.txt")
 
-  #FindAndWriteArticle("http://www.playgroundsforeveryone.com/sitemap.xml", "npr_article.txt")
+  FindAndWriteArticle(front_page="https://www.npr.org/sections/politics/", article_name="npr_article.txt")
 
-  FindAndWriteArticle("http://www.foxnews.com/sitemap.xml?idx=26", "foxnews_article.txt")
+  ##FindAndWriteArticle("http://www.foxnews.com/sitemap.xml?idx=26", "foxnews_article.txt")
 
-  FindAndWriteArticle("https://www.bloomberg.com/feeds/bpol/sitemap_news.xml", "bloomberg_article.txt")
+  ##FindAndWriteArticle("https://www.bloomberg.com/feeds/bpol/sitemap_news.xml", "bloomberg_article.txt")
